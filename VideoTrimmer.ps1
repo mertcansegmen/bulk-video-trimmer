ba15b94e4d4e877e5d0147b8a29b5471874a87f0
Mertcan <mertcan.segmen@hotmail.com>
2021-04-18 02:21:36 +0300

    removed some unnecessary stuff

--- VideoTrimmer.ps1
+++ VideoTrimmer.ps1
@@ -13,12 +13,12 @@ param (
 
 $Mp4FilePaths = Get-ChildItem -Path $FolderPath -Include $InputFormats -Recurse
 
-foreach($Mp4FilePath in $Mp4FilePaths) {
+foreach ($Mp4FilePath in $Mp4FilePaths) {
     $FileName = (Get-Item $Mp4FilePath).Basename
     $FileInfo = "$FileName" -Split ","
     $VideoName = $FileInfo[0].Trim()
-    $Start = "00:" + $FileInfo[1].Trim().Replace(".", ":")
-    $End = "00:" + $FileInfo[2].Trim().Replace(".", ":")
+    $Start = $FileInfo[1].Trim().Replace(".", ":")
+    $End = $FileInfo[2].Trim().Replace(".", ":")
 
     $OutputFilePath = "$OutputFolder/$VideoName.$OutputFormat"
 
@@ -32,12 +32,12 @@ foreach($Mp4FilePath in $Mp4FilePaths) {
     $LastAccessTime = (Get-Item $Mp4FilePath).LastAccessTime.ToString("yyyy-MM-dd HH:mm:ss")
     $LastWriteTime = (Get-Item $Mp4FilePath).LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss")
 
-    $(Get-Item $OutputFilePath).CreationTime = $CreationTime
-    $(Get-Item $OutputFilePath).LastAccessTime = $LastAccessTime
-    $(Get-Item $OutputFilePath).LastWriteTime = $LastWriteTime
+    (Get-Item $OutputFilePath).CreationTime = $CreationTime
+    (Get-Item $OutputFilePath).LastAccessTime = $LastAccessTime
+    (Get-Item $OutputFilePath).LastWriteTime = $LastWriteTime
 }
 
-If($DeleteAfterGeneration) {
+if ($DeleteAfterGeneration) {
     foreach($Mp4FilePath in $Mp4FilePaths) {
         Remove-Item $Mp4FilePath
     }
