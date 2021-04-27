828454388c21bf12913045d6b962e8a87819e007
Mertcan <mertcan.segmen@hotmail.com>
2021-04-28 02:09:27 +0300

    added overwriting option

--- VideoTrimmer.ps1
+++ VideoTrimmer.ps1
@@ -8,7 +8,9 @@ param (
     [Parameter(Mandatory=$false)]
     [string[]]$InputFormats = "*mp4",
     [Parameter(Mandatory=$false)]
-    [string[]]$OutputFormat
+    [string[]]$OutputFormat,
+    [Parameter(Mandatory=$false)]
+    [switch]$DontOverwrite
 )
 
 $Mp4FilePaths = Get-ChildItem -Path $FolderPath -Include $InputFormats -Recurse
@@ -20,6 +22,7 @@ foreach ($Mp4FilePath in $Mp4FilePaths) {
     $Start = $FileInfo[1].Trim().Replace(".", ":")
     $End = $FileInfo[2].Trim().Replace(".", ":")
     $Format = If ($OutputFormat) {".$OutputFormat"} Else {(Get-Item $Mp4FilePath).Extension}
+    $Overwrite = If ($DontOverwrite) {"-n"} Else {"-y"}
 
     $OutputFilePath = "$OutputFolder/$VideoName$Format"
 
@@ -27,7 +30,8 @@ foreach ($Mp4FilePath in $Mp4FilePaths) {
             -to $End `
             -i "$Mp4FilePath" `
             -vcodec libx265 `
-            -crf 28 $OutputFilePath
+            -crf 28 $OutputFilePath `
+            $Overwrite
 
     $CreationTime = (Get-Item $Mp4FilePath).CreationTime.ToString("yyyy-MM-dd HH:mm:ss")
     $LastAccessTime = (Get-Item $Mp4FilePath).LastAccessTime.ToString("yyyy-MM-dd HH:mm:ss")
